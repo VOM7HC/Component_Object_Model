427e9937a6741f847d80f23081dee4045d8a8dd5
Khoi Vo Minh <khoiminhvo1996@hotmail.com>
2025-01-01 23:49:48 +0700

    Init Project

--- COM_Essentials_One/COM_Essentials_One.cpp
+++ COM_Essentials_One/COM_Essentials_One.cpp
@@ -10,6 +10,39 @@
 #define TRACE(s) OutputDebugString(s)
 #define ASSERT _ASSERTE
 
+template<typename T>
+class RemoveAddRefRelease : public T
+{
+public:
+	ULONG _stdcall AddRef() { return 1; }
+	ULONG _stdcall Release() { return 1; }
+};
+
+template<typename T>
+class ComPtr
+{
+private:
+	T* m_ptr;
+public:
+	ComPtr() : m_ptr(nullptr) {}
+	//ComPtr(T* const ptr) : m_ptr(ptr) {}
+	~ComPtr()
+	{
+		if (m_ptr)
+			m_ptr->Release();
+	}
+	RemoveAddRefRelease<T>* operator->() const
+	{
+		ASSERT(m_ptr);
+		return static_cast<RemoveAddRefRelease<T>*> (m_ptr);
+	}
+	//T& operator*() { return *m_ptr; }
+	T** GetAddressOf()
+	{
+		return &m_ptr;
+	}
+};
+
 struct ComException
 {
 	HRESULT hr;
@@ -27,31 +60,37 @@ inline void HR(HRESULT const hr)
 int main()
 {
 	// Example 1
-	IHen* hen;
+	//IHen* hen;
+	ComPtr<IHen> hen;
 
-	if (S_OK != CreateHen(&hen))
+	//if (S_OK != CreateHen(&hen))
+    if (S_OK != CreateHen(hen.GetAddressOf()))
 	{
 		return 0;
 	}
 
 	hen->Cluck();
 
-	IHen2* hen2;
-	if (S_OK == hen->QueryInterface(__uuidof(IHen2), reinterpret_cast<void**>(&hen2)))
+	//IHen2* hen2;
+	ComPtr<IHen2> hen2;
+	//if (S_OK == hen->QueryInterface(__uuidof(IHen2), reinterpret_cast<void**>(&hen2)))
+	if (S_OK == hen->QueryInterface(__uuidof(IHen2), reinterpret_cast<void**>(hen2.GetAddressOf())))
 	{
 		hen2->Forage();
-		hen2->Release();
+		//hen2->Release();
 	}
 
-	IOfflineChicken* offlineChicken;
-	if (S_OK == hen->QueryInterface(__uuidof(IOfflineChicken), reinterpret_cast<void**>(&offlineChicken)))
+	//IOfflineChicken* offlineChicken;
+	ComPtr<IOfflineChicken> offlineChicken;
+	//if (S_OK == hen->QueryInterface(__uuidof(IOfflineChicken), reinterpret_cast<void**>(&offlineChicken)))
+	if (S_OK == hen->QueryInterface(__uuidof(IOfflineChicken), reinterpret_cast<void**>(offlineChicken.GetAddressOf())))
 	{
 		offlineChicken->Load("filename");
 		offlineChicken->Save("filename");
-		offlineChicken->Release();
+		//offlineChicken->Release();
 	}
 
-	hen->Release();
+	//hen->Release();
 
 	return 0;
 }
